662981d2867f11172d3283d7eac1ecd62a6bd8af
Jacob Shing <zhanhoshing@icloud.com>
2025-02-03 22:49:44 +0800

    fix: ToString() function

--- src/SFloat/SFloat.cs
+++ src/SFloat/SFloat.cs
@@ -344,14 +344,27 @@ public struct SFloat {
     }
 
     public override string ToString() {
-        var chars = new char[_digits.Length + 2];
-        if (_isNegative) chars[0] = '-';
-        for (var i = 0; i < _digits.Length; i++) {
-            if (i <= _floatPointIndex) chars[_isNegative ? i + 1 : i] = _digits[i];       // Before the float point.
-            if (FractionLength == 0) break;
-            if (i == _floatPointIndex) chars[_isNegative ? i + 1 : i] = '.';             // At the float point.
-            else chars[_isNegative ? i + 2 : i + 1]                        = _digits[i]; // After the float point.
+        // var chars = new char[_digits.Length + 2];
+        // if (_isNegative) chars[0] = '-';
+        // for (var i = 0; i < _digits.Length; i++) {
+        //     if (i <= _floatPointIndex) chars[_isNegative ? i + 1 : i] = _digits[i];       // Before the float point.
+        //     if (i == _floatPointIndex && FractionLength == 0) break;
+        //     if (i == _floatPointIndex) chars[_isNegative ? i + 1 : i] = '.';             // At the float point.
+        //     else chars[_isNegative ? i + 2 : i + 1]                        = _digits[i]; // After the float point.
+        // }
+        // return new string(chars).Trim('\0');
+
+        var intChars = new char[IntegerLength];
+        for (var i = IntegerLength - 1; i >= 0; i--) {
+            intChars[IntegerLength - i - 1] = GetDigitAt(i);
         }
-        return new string(chars).Trim('\0');
+        if (FractionLength == 0)
+            return $"{(_isNegative ? "-" : "")}{new string(intChars)}";
+        
+        var fracChars = new char[FractionLength];
+        for (var i = 1; i <= FractionLength; i++) {
+            fracChars[i - 1] = GetDigitAt(-i);
+        }
+        return $"{(_isNegative ? "-" : "")}{new string(intChars)}.{new string(fracChars)}";
     }
 }
