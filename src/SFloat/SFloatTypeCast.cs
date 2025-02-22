6a6f2753fd77de12ea6758718d8aa2fd0d8f4d42
Jacob Shing <zhanhoshing@icloud.com>
2025-02-22 22:12:33 +0800

    fix: incorrect type case from SFloat to int

--- src/SFloat/SFloatTypeCast.cs
+++ src/SFloat/SFloatTypeCast.cs
@@ -14,12 +14,16 @@ public readonly partial struct SFloat {
     public static implicit operator SFloat(int i) => new (i.ToString());
 
     public static implicit operator int(SFloat flt) {
+        // For a number (a_n-1 a_n-2 ... a_1 a_0)_r, the value in decimal is:
+        //      r * (r * (r * a_n-1 + a_n-2) + a_n-3) + ... + a_1) + a_0
+        
         // Any fractional part will be dropped directly.
         try {
-            var result = 0;
-            for (var i = flt.IntegerLength; i >= 0; i--) {
-                result = checked(result + GetDigitValue(flt.GetDigitAt(i)) * flt.Radix +
-                                 GetDigitValue(flt.GetDigitAt(i - 1)));
+            var result = GetDigitValue(flt.GetDigitAt(flt.IntegerLength - 1)); // a_n-1
+            for (var i = flt.IntegerLength - 1; i > 0; i--) {
+                checked {
+                    result = result * flt.Radix + GetDigitValue(flt.GetDigitAt(i - 1));
+                }
             }
 
             return flt.IsNegative ? -result : result;
